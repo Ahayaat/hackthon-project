2b049d79ba07de1296b094f2ea4d464b2be83814
Ahayaat <amala.hayat55@gmail.com>
2023-08-25 22:53:01 +0500

    add

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,3 +1,6 @@
-const BASE_PATH_FORAPI = "http://localhost:3000";
+const BASE_PATH_FORAPI =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://hackathon-project-hn30eci13-ahayaat.vercel.app";
 
 export default BASE_PATH_FORAPI;
